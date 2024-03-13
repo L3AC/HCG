dee8ea769f64129125ac755e0aabb380d08151c9
L3AC <101782981+L3AC@users.noreply.github.com>
2024-03-12 21:03:50 -0600

    Create HGC_DB.sql

---
+++ HGC_DB.sql
@@ -0,0 +1,15 @@
+DROP DATABASE db_hgc;
+CREATE DATABASE IF NOT EXISTS db_hgc;
+USE db_hgc;
+
+CREATE TABLE tb_usuarios();
+CREATE TABLE tb_clientes();
+
+CREATE TABLE tb_tipo_productos();
+CREATE TABLE tb_productos();
+
+CREATE TABLE tb_menus();
+CREATE TABLE tb_menu_productos();
+
+CREATE TABLE tb_pedidos();
+CREATE TABLE tb_detalle_pedidos();
