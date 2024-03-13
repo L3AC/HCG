f9a7c6a390d36f70ade09270f18f939ca6a22cca
L3AC <101782981+L3AC@users.noreply.github.com>
2024-03-12 21:07:29 -0600

    Update HGC.sql

--- HGC.sql
+++ HGC.sql
@@ -3,26 +3,12 @@ CREATE DATABASE IF NOT EXISTS db_hgc;
 USE db_hgc;
 
 CREATE TABLE tb_roles(
-  id_rol INT UNSIGNED AUTO_INCREMENT,
-  descripcion_opc VARCHAR(40) NOT NULL,
-  estado_opc BOOLEAN NOT NULL,
-  marcas_opc BOOLEAN NOT NULL,
-  modelos_opc BOOLEAN NOT NULL,
-  tallas_opc BOOLEAN NOT NULL,
-  pedidos_opc BOOLEAN NOT NULL,
-  tipo_noticias_opc BOOLEAN NOT NULL,
-  noticias_opc BOOLEAN NOT NULL,
-  comentarios_opc BOOLEAN NOT NULL,
-  clientes_opc BOOLEAN NOT NULL,
-  usuarios_opc BOOLEAN NOT NULL,
-  roles_opc BOOLEAN NOT NULL,
-  PRIMARY KEY(id_rol)
 );
 
 CREATE TABLE tb_usuarios(
   id_usuario INT UNSIGNED auto_increment,
   id_rol INT UNSIGNED NOT NULL,
-  usuario_usuario VARCHAR(30) UNIQUE NOT NULL,
+  alias_usuario VARCHAR(30) UNIQUE NOT NULL,
   clave_usuario VARCHAR(255) NOT NULL,
   nombre_usuario VARCHAR(255) NOT NULL,
   apellido_usuario VARCHAR(255) NOT NULL,
