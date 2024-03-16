d8cb495bac476fc2fb73e3291d2f501e55a55672
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-03-16 17:28:56 -0600

    C

--- HGC.sql
+++ HGC.sql
@@ -27,36 +27,42 @@ CREATE TABLE tb_usuarios(
 
 CREATE TABLE tb_clientes(
   id_cliente INT UNSIGNED auto_increment,
-  alias_cliente VARCHAR(30) UNIQUE NOT NULL,
-  clave_cliente VARCHAR(255) NOT NULL,
   telefono_cliente VARCHAR(255) NOT NULL,
   nombre_cliente VARCHAR(255) NOT NULL,
   apellido_cliente VARCHAR(255) NOT NULL,
   email_cliente VARCHAR(255) NOT NULL,
-  pin_cliente VARCHAR(6) NOT NULL,
-  estado_cliente BOOLEAN DEFAULT TRUE,
   PRIMARY KEY (id_cliente)
 );
+/*ESTO VA A SERVIR PARA INGRESAR POR EJEMPLO, CATEGORIA DE BEBIDAS, SNACKS, POSTRE, PORCION DE UN PLATILLO*/
+CREATE TABLE tb_tipo_items(
+  id_tipo_item INT UNSIGNED auto_increment,
+  descripcion_tipo_item VARCHAR(255) NOT NULL,
+  estado_tipo_item BOOLEAN DEFAULT TRUE,
+  PRIMARY KEY (id_tipo_item)
+);
 
-
+CREATE TABLE tb_items(
+  id_item INT UNSIGNED auto_increment,
+  id_tipo_item INT UNSIGNED ,/*BEBIDA, PLATO, SNACK,*/
+  nombre_item VARCHAR(255) NOT NULL,
+  descripcion_item VARCHAR(255)  NOT NULL,
+  precio_item DECIMAL(10, 2) NOT NULL,
+  estado_item BOOLEAN DEFAULT TRUE,
+  PRIMARY KEY (id_item),
+  CONSTRAINT fk_item_tipo 
+  FOREIGN KEY(id_tipo_item) REFERENCES tb_tipo_items(id_tipo_item)
+);
+/*ESTO VA A SERVIR PARA AGREGAR EL TIPO DE PRODUCTO QUE VA A SER POR EJEMPLO
+SI ES UN COMBO(plato de gallina, combo hamburguesa que lleva soda, papas,hamburguesa ),
+SI ES UN PRODUCTO COMPLEMENTARIO(solo soda, solo una botella de agua, etc)
+SI ES UN TIPICO
 CREATE TABLE tb_tipo_productos(
   id_tipo_producto INT UNSIGNED auto_increment,
   descripcion_tipo_producto VARCHAR(255) NOT NULL,
   estado_tipo_producto BOOLEAN DEFAULT TRUE,
   PRIMARY KEY (id_tipo_producto)
-);
+);*/
 
-CREATE TABLE tb_items(
-  id_item INT UNSIGNED auto_increment,
-  #id_tipo_item INT UNSIGNED ,/*BEBIDA, PLATO, SNACK,*/
-  nombre_item VARCHAR(255) NOT NULL,
-  descripcion_item VARCHAR(255)  NOT NULL,
-  precio_item DECIMAL(10, 2) NOT NULL,
-  estado_item BOOLEAN DEFAULT TRUE,
-  PRIMARY KEY (id_item)
-  /*CONSTRAINT fk_item_tipo 
-  FOREIGN KEY(id_tipo_item) REFERENCES tb_tipo_items(id_tipo_item)*/
-);
 
 /*D = Desayuno, A = Almuerzo, C = Cena, T = Tipico,TD = Todo el dia
   D-A = Desayuno y Almuerzo, D-C = Desayuno y Cena, A-C = Almuerzo y Cena
@@ -64,7 +70,7 @@ CREATE TABLE tb_items(
 */
 CREATE TABLE tb_productos(
   id_producto INT UNSIGNED auto_increment,
-  id_tipo_producto INT UNSIGNED,
+  tipo_producto ENUM('Conjunto','Complementario') NOT NULL,
   descripcion_producto VARCHAR(255) NOT NULL,
   horario_producto ENUM('D','A','C','T','TD','D-A','D-C','A-C','T-D','TA','T-C') NOT NULL,
   precio_producto DECIMAL(10, 2) NOT NULL,
@@ -75,9 +81,10 @@ CREATE TABLE tb_productos(
   viernes_producto BOOLEAN NOT NULL,
   sabado_producto BOOLEAN NOT NULL,
   domingo_producto BOOLEAN NOT NULL,
-  PRIMARY KEY (id_producto),
+  PRIMARY KEY (id_producto)/*,
   CONSTRAINT fk_tipo_producto
-  FOREIGN KEY(id_tipo_producto) REFERENCES tb_tipo_productos(id_tipo_producto) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(id_tipo_producto) REFERENCES tb_tipo_productos(id_tipo_producto) 
+  ON DELETE CASCADE ON UPDATE CASCADE*/
 );
 
 
@@ -85,12 +92,21 @@ CREATE TABLE tb_detalle_productos(
   id_detalle_producto INT UNSIGNED auto_increment,
   id_item INT UNSIGNED  NOT NULL,
   id_producto INT UNSIGNED  NOT NULL, /*TRAE EL ID DEL producto*/
+  cantidad_item INT UNSIGNED NOT NULL,
   PRIMARY KEY (id_detalle_producto),
   CONSTRAINT fk_item_producto_item /*LLAVE FORANEA*/
   FOREIGN KEY(id_item) REFERENCES tb_items(id_item)
   ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT fk_item_producto_producto /*LLAVE FORANEA*/
   FOREIGN KEY(id_producto) REFERENCES tb_productos(id_producto) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE tb_clientes(
+  id_cliente INT UNSIGNED auto_increment,
+  telefono_cliente VARCHAR(255) NOT NULL,
+  nombre_cliente VARCHAR(255) NOT NULL,
+  apellido_cliente VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_cliente)
 );
 
 CREATE TABLE tb_pedidos(
@@ -107,7 +123,7 @@ CREATE TABLE tb_detalle_pedidos (
   id_detalle_pedido INT UNSIGNED AUTO_INCREMENT,
   id_pedido INT UNSIGNED NOT NULL,
   id_producto INT UNSIGNED NOT NULL,
-  cantidad INT UNSIGNED NOT NULL,
+  cantidad_pedido INT UNSIGNED NOT NULL,
   PRIMARY KEY (id_detalle_pedido),
   FOREIGN KEY (id_pedido) REFERENCES tb_pedidos(id_pedido) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (id_producto) REFERENCES tb_productos(id_producto) ON DELETE CASCADE ON UPDATE CASCADE
