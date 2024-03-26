b80a3581e179a75eb2f4c7f118765156e41e313f
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-03-25 20:12:00 -0600

    update 7 files

--- api/models/data/admin/1productos_data.php
+++ api/models/data/admin/1productos_data.php
@@ -2,7 +2,7 @@
 // Se incluye la clase para validar los datos de entrada.
 require_once('../../helpers/validator.php');
 // Se incluye la clase padre.
-require_once('../../models/handler/admin/1productos_handler_admin.php');
+require_once('../../models/handler/admin/1productos_handler.php');
 /*
  *	Clase para manejar el encapsulamiento de los datos de la tabla PRODUCTO.
  */
--- api/models/data/admin/8detalleproductos_data.php
+++ api/models/data/admin/8detalleproductos_data.php
@@ -2,7 +2,7 @@
 // Se incluye la clase para validar los datos de entrada.
 require_once('../../helpers/validator.php');
 // Se incluye la clase padre.
-require_once('../../handler/admin/8detalleproductos_handler.php');
+require_once('../../models/handler/admin/8detalleproductos_handler.php');
 
 /*
  *	Clase para manejar el encapsulamiento de los datos de la tabla PRODUCTO.
