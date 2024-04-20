e5006b73b9290b8708738a98b71a684d39abd778
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-04-19 22:34:41 -0600

    update 6usuarios.js and 6usuarios.html

--- controllers/admin/6usuarios.js
+++ controllers/admin/6usuarios.js
@@ -148,14 +148,14 @@ const fillTable = async (form = null) => {
             (row.estado_usuario) ? icon = 'bi bi-eye-fill' : icon = 'bi bi-eye-slash-fill';
             // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             TABLE_BODY.innerHTML += `
-            <div class="cardlar row" style="margin-bottom: 10px; margin-left: auto; margin-right: auto;">
+            <div class="cardlar row col-12" style="margin-bottom: 10px; margin-left: auto; margin-right: auto;">
             <div class="col-lg-2 col-md-12 col-sm-12" style="display: flex; align-items: center; font-size: 17px;"><div class="texto-antes">Apellido: </div>${row.apellido_usuario}</div>
             <div class="col-lg-2 col-md-12 col-sm-12" style="display: flex; align-items: center; font-size: 17px;"><div class="texto-antes">Nombre: </div>${row.nombre_usuario}</div>
             <div class="col-lg-3  col-sm-12" style="display: flex; align-items: center; font-size: 17px;"><div class="texto-antes">Correo: </div>${row.email_usuario}</div>
             <div class="col-lg-2 col-md-12 col-sm-12" style="display: flex; align-items: center; font-size: 17px;">
-    <div class="texto-antes">Usuario: </div>
-    <div class="ms-md-4 d-md-inline">${row.alias_usuario}</div>
-</div>
+                <div class="texto-antes">Usuario: </div>
+                    <div class="ms-md-4 d-md-inline">${row.alias_usuario}</div>
+                </div>
 
                 <div class="col-lg-1 col-md-12 col-sm-12" style="display: flex; align-items: center;font-size: 30px;"><div class="texto-antes" style="font-size: 17px;">Estado: </div><i  class="${icon}"></i></div>
                 <div class="col-lg-2 col-md-12 col-sm-12 d-flex justify-content-end">
