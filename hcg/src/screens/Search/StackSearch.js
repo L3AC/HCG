08845ece36c207d4dae87eb8d0bccd1b251bf282
L3AC <leac.2xy@gmail.com>
2024-05-27 13:41:44 -0600

    build(deps): update package-lock.json

---
+++ hcg/src/screens/Search/StackSearch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Explorar from '../../screens/Logged/Explorar';
+import Modelo from './Modelo'; 
+
+const Stack = createStackNavigator();
+
+const StackCart = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Explorar" component={Explorar} options={{ headerShown: false }} />
+      <Stack.Screen name="Modelo" component={Modelo} options={{ headerShown: false }} /> 
+    </Stack.Navigator>
+  );
+};
+
+export default StackCart;
