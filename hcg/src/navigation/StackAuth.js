f687b5d3855abfd63c38c5e9917ed80a52dd2318
L3AC <leac.2xy@gmail.com>
2024-05-31 10:43:37 -0600

    update StackAuth.js

--- hcg/src/navigation/StackAuth.js
+++ hcg/src/navigation/StackAuth.js
@@ -3,17 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './src/screens/NotLogged/Login';
+import SignUp from './src/screens/NotLogged/SignUp';
 import NuevaClave from './src/screens/NotLogged/NuevaClave';
 import VerifCode from './src/screens/NotLogged/VerifCode';
 import VerifUs from './src/screens/NotLogged/VerifUs';
 
 const Stack = createStackNavigator();
 
-const AuthStack = () => (
+const StackAuth = () => (
     <Stack.Navigator>
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
       <Stack.Screen name="NuevaClave" component={NuevaClave} options={{ headerShown: false }} />
       <Stack.Screen name="VerifCode" component={VerifCode} options={{ headerShown: false }} />
       <Stack.Screen name="VerifUs" component={VerifUs} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
+
+export default StackAuth;
