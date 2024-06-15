c6939414a49c3b91433b864ab33e445644d4954d
Frank5031 <20220332@ricaldone.edu.sv>
2024-06-15 17:01:24 -0600

    Agregar comentarios de la ventana de pefil

--- hcg/src/screens/Logged/Perfil.js
+++ hcg/src/screens/Logged/Perfil.js
@@ -1,20 +1,23 @@
+// Importación de librerías y componentes necesarios
 import React, { useState, useEffect, useCallback } from 'react';
 import { ScrollView, RefreshControl, StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { SERVER } from '../../contexts/Network';
-import { useRoute, useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons'; // Iconos de Ionicons
+import { SERVER } from '../../contexts/Network'; // URL del servidor
+import { useRoute, useNavigation } from '@react-navigation/native'; // Hook de navegación
 
+// Componente de función PerfilScreen
 const PerfilScreen = () => {
-  const navigation = useNavigation();
-  const [refreshing, setRefreshing] = useState(false);
+  const navigation = useNavigation(); // Hook de navegación para cambiar entre pantallas
+  const [refreshing, setRefreshing] = useState(false); // Estado para control de la actualización
   const [profileData, setProfileData] = useState({
     nombre_cliente: '',
     apellido_cliente: '',
     correo_cliente: '',
     usuario_cliente: '',
     telefono_cliente: ''
-  });
+  }); // Estado para almacenar los datos del perfil
 
+  // Función para obtener los datos del perfil
   const fetchProfileData = async () => {
     try {
       const response = await fetch(`${SERVER}services/public/clientes.php?action=readProfile`, {
@@ -22,35 +25,40 @@ const PerfilScreen = () => {
       });
       const data = await response.json();
       if (data.status) {
-        setProfileData(data.dataset);
+        setProfileData(data.dataset); // Actualiza los datos del perfil
       } else {
-        Alert.alert('Error', 'Failed to fetch profile data');
+        Alert.alert('Error', 'Failed to fetch profile data'); // Muestra un alerta en caso de error
       }
     } catch (error) {
       console.error(error, "Error desde Catch");
-      Alert.alert('Error', 'Ocurrió un error al iniciar sesión');
+      Alert.alert('Error', 'Ocurrió un error al iniciar sesión'); // Muestra un alerta en caso de error
     }
   };
 
+  // useEffect para obtener los datos del perfil al montar el componente
   useEffect(() => {
     fetchProfileData();
   }, []);
 
+  // Función de actualización
   const onRefresh = useCallback(() => {
     setRefreshing(true);
     fetchProfileData().finally(() => setRefreshing(false));
   }, []);
 
   return (
+    // Contenedor de desplazamiento con control de actualización
     <ScrollView
       contentContainerStyle={styles.container}
       refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
     >
+      {/* Encabezado con íconos de retroceso y edición */}
       <View style={styles.header}>
         <Ionicons name="arrow-back" size={35} color="black" onPress={() => navigation.goBack()}/>
         <Text style={styles.title}>Perfil</Text>
         <Ionicons name="pencil" size={35} color="black" />
       </View>
+      {/* Formulario para los datos del perfil */}
       <View style={styles.form}>
         <View style={styles.inputContainer}>
           <Text style={styles.label}>Nombre</Text>
@@ -77,7 +85,7 @@ const PerfilScreen = () => {
             placeholder="Correo" 
             value={profileData.correo_cliente} 
             onChangeText={(text) => setProfileData({ ...profileData, correo_cliente: text })}
-            keyboardType="email-address"
+            keyboardType="email-address" // Teclado específico para direcciones de correo
           />
         </View>
         <View style={styles.inputContainer}>
@@ -96,10 +104,11 @@ const PerfilScreen = () => {
             placeholder="Telefono" 
             value={profileData.telefono_cliente} 
             onChangeText={(text) => setProfileData({ ...profileData, telefono_cliente: text })}
-            keyboardType="phone-pad"
+            keyboardType="phone-pad" // Teclado específico para números de teléfono
           />
         </View>
       </View>
+      {/* Botón para guardar los cambios */}
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Guardar</Text>
       </TouchableOpacity>
@@ -107,56 +116,57 @@ const PerfilScreen = () => {
   );
 };
 
+// Estilos del componente
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
+    flexGrow: 1, // Permite que el contenido se expanda y se desplace
     padding: 16,
-    backgroundColor: '#d2a563',
+    backgroundColor: '#d2a563', // Color de fondo
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: 'row', // Coloca los elementos en una fila
+    justifyContent: 'space-between', // Espacia los elementos de manera equitativa
+    alignItems: 'center', // Alinea los elementos verticalmente al centro
     marginBottom: 30,
     marginTop: 45,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: 'black',
+    color: 'black', // Color del texto del título
   },
   form: {
     flex: 1,
   },
   inputContainer: {
-    marginBottom: 25,
+    marginBottom: 25, // Margen inferior entre los contenedores de entrada
   },
   label: {
     fontSize: 16,
-    color: 'black',
+    color: 'black', // Color del texto de las etiquetas
     marginBottom: 15,
   },
   input: {
     height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    paddingHorizontal: 8,
-    borderRadius: 8,
-    backgroundColor: '#F2E7CF',
+    borderColor: '#ccc', // Color del borde del input
+    borderWidth: 1, // Ancho del borde del input
+    paddingHorizontal: 8, // Relleno horizontal dentro del input
+    borderRadius: 8, // Bordes redondeados
+    backgroundColor: '#F2E7CF', // Color de fondo del input
   },
   button: {
     height: 50,
     width: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-    borderRadius: 8,
-    marginTop: 0,
+    justifyContent: 'center', // Centra el contenido verticalmente
+    alignItems: 'center', // Centra el contenido horizontalmente
+    backgroundColor: 'black', // Color de fondo del botón
+    borderRadius: 8, // Bordes redondeados del botón
+    marginTop: 0, // Margen superior del botón
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: 'white', // Color del texto del botón
+    fontWeight: 'bold', // Peso de la fuente (negrita)
   },
 });
 
-export default PerfilScreen;
+export default PerfilScreen; // Exporta el componente PerfilScreen
