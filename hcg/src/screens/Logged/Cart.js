76ef3395f58bdc80b119926f8974e8cb7ac517ee
Frank5031 <20220332@ricaldone.edu.sv>
2024-06-15 17:48:18 -0600

    Comentarios para la ventana del carrito

--- hcg/src/screens/Logged/Cart.js
+++ hcg/src/screens/Logged/Cart.js
@@ -5,20 +5,20 @@ import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 
 const CartScreen = () => {
-  const [refreshing, setRefreshing] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [cantidad, setCantidad] = useState('');
-  const [nota, setNota] = useState('');
-  const [currentItemId, setCurrentItemId] = useState(null);
-  const [loadingModal, setLoadingModal] = useState(false);
-  const navigation = useNavigation();
+  const [refreshing, setRefreshing] = useState(false); // Estado para indicar si se está refrescando la pantalla
+  const [loading, setLoading] = useState(false); // Estado para indicar si se está cargando la lista de elementos del carrito
+  const [cartItems, setCartItems] = useState([]); // Estado para almacenar los elementos del carrito
+  const [modalVisible, setModalVisible] = useState(false); // Estado para controlar la visibilidad del modal
+  const [cantidad, setCantidad] = useState(''); // Estado para almacenar la cantidad de productos del modal
+  const [nota, setNota] = useState(''); // Estado para almacenar la nota del producto del modal
+  const [currentItemId, setCurrentItemId] = useState(null); // Estado para almacenar el ID del elemento actual del modal
+  const [loadingModal, setLoadingModal] = useState(false); // Estado para indicar si se está cargando el modal
+  const navigation = useNavigation(); // Hook de navegación para acceder a la navegación
 
-
+  // Función para obtener los datos del carrito desde el servidor
   const fetchMenuData = async (query = '') => {
     try {
-      setLoading(true);
+      setLoading(true); // Indica que se está cargando
       const formData = new FormData();
       formData.append('producto', query);
       const response = await fetch(`${SERVER}services/public/pedidos.php?action=readDetail`, {
@@ -28,22 +28,24 @@ const CartScreen = () => {
       const data = await response.json();
 
       if (response.ok && data.status === 1) {
-        setCartItems(data.dataset || []);
+        setCartItems(data.dataset || []); // Actualiza los elementos del carrito
       } else {
-        Alert.alert('No hay ningún producto agregado');
-        navigation.navigate('StackHome');
+        Alert.alert('No hay ningún producto agregado'); // Muestra una alerta si no hay productos
+        navigation.navigate('StackHome'); // Navega a la pantalla principal (home)
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error:', error); // Muestra un error en la consola en caso de fallo
     } finally {
-      setLoading(false);
-      setRefreshing(false);
+      setLoading(false); // Finaliza el estado de carga
+      setRefreshing(false); // Finaliza el estado de refresco
     }
   };
+
+  // Función para obtener los detalles de un producto específico
   const readOne = async (idDetallePedido) => {
     try {
-      setLoadingModal(true);
-      const formData = new FormData();
+      setLoadingModal(true); // Indica que se está cargando el modal
+      const formData = new FormData(); 
       formData.append('idDetallePedido', idDetallePedido);
       const response = await fetch(`${SERVER}services/public/detallepedidos.php?action=readOne`, {
         method: 'POST',
@@ -52,26 +54,27 @@ const CartScreen = () => {
       const data = await response.json();
 
       if (response.ok && data.status === 1) {
-        setCantidad(data.dataset.cantidad_pedido.toString() || '1');
-        setNota(data.dataset.nota_pedido || 'Nota vacía');
-        setCurrentItemId(idDetallePedido);
-        console.log(cantidad + ' ' + nota + idDetallePedido);
+        setCantidad(data.dataset.cantidad_pedido.toString() || '1'); // Actualiza la cantidad del producto
+        setNota(data.dataset.nota_pedido || 'Nota vacía'); // Actualiza la nota del producto
+        setCurrentItemId(idDetallePedido); // Actualiza el ID del producto actual
+        console.log(cantidad + ' ' + nota + idDetallePedido); // Muestra el modal
         setModalVisible(true);
       } else {
-        console.error('Error fetching data:', data.error);
-        Alert.alert('Error', data.error);
+        console.error('Error fetching data:', data.error); // Muestra un error si falla la obtención de datos
+        Alert.alert('Error', data.error); // Muestra una alerta con el mensaje de error
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error:', error); // Muestra un error en la consola en caso de fallo
     } finally {
-      setLoadingModal(false);
+      setLoadingModal(false); // Finaliza el estado de carga del modal
     }
   };
 
+  // Función para actualizar los detalles de un producto
   const updateDetalle = async (idDetallePedido) => {
     try {
-      setLoading(true);
-      const formData = new FormData();
+      setLoading(true); // Indica que se está cargando
+      const formData = new FormData(); 
       formData.append('idDetallePedido', idDetallePedido);
       formData.append('cantidadPedido', cantidad);
       formData.append('notaPedido', nota);
@@ -82,19 +85,21 @@ const CartScreen = () => {
       const data = await response.json();
 
       if (response.ok && data.status === 1) {
-        fetchMenuData(); // Refresh the cart items after update
-        setModalVisible(false);
+        fetchMenuData(); // Actualiza los elementos del carrito después de la actualización
+        setModalVisible(false); // Oculta el modal
       } else {
-        console.error('Error updating data:', data.error);
-        Alert.alert('Error', data.error);
+        console.error('Error updating data:', data.error); // Muestra un error si falla la actualización
+        Alert.alert('Error', data.error); // Muestra una alerta con el mensaje de error
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error:', error); // Muestra un error en la consola en caso de fallo
     } finally {
-      setLoading(false);
-      setRefreshing(false);
+      setLoading(false); // Finaliza el estado de carga
+      setRefreshing(false); // Finaliza el estado de refresco
     }
   };
+
+  // Función para eliminar un detalle de pedido
   const deleteDetalle = async (idDetallePedido) => {
     Alert.alert(
       "Confirmar eliminación",
@@ -109,7 +114,7 @@ const CartScreen = () => {
           text: "Eliminar",
           onPress: async () => {
             try {
-              setLoading(true);
+              setLoading(true); // Indica que se está cargando
               const formData = new FormData();
               formData.append('idDetallePedido', idDetallePedido);
               const response = await fetch(`${SERVER}services/public/detallepedidos.php?action=deleteRow`, {
@@ -119,16 +124,16 @@ const CartScreen = () => {
               const data = await response.json();
 
               if (response.ok && data.status === 1) {
-                fetchMenuData(); // Refresh the cart items after update
-                setModalVisible(false);
+                fetchMenuData(); // Actualiza los elementos del carrito después de la eliminación
+                setModalVisible(false); // Oculta el modal
               } else {
-                Alert.alert('Error', data.error);
+                Alert.alert('Error', data.error); // Muestra una alerta con el mensaje de error
               }
             } catch (error) {
-              console.error('Error:', error);
+              console.error('Error:', error); // Muestra un error en la consola en caso de fallo
             } finally {
-              setLoading(false);
-              setRefreshing(false);
+              setLoading(false); // Finaliza el estado de carga
+              setRefreshing(false); // Finaliza el estado de refresco
             }
           },
         },
@@ -137,44 +142,47 @@ const CartScreen = () => {
     );
   };
 
+  // Función para finalizar el pedido
   const finishOrder = async (idDetallePedido) => {
     try {
-      setLoading(true);
+      setLoading(true); // Indica que se está cargando
       const response = await fetch(`${SERVER}services/public/pedidos.php?action=finishOrder`, {
         method: 'POST'
       });
       const data = await response.json();
 
       if (response.ok && data.status === 1) {
-        Alert.alert('Error', data.error);
-        navigation.navigate('Home');
+        Alert.alert('Error', data.error); // Muestra una alerta con el mensaje de error
+        navigation.navigate('Home'); // Navega a la pantalla de inicio
       } else {
-        console.error('Error:', data.error);
-        Alert.alert('Error', data.error);
+        console.error('Error:', data.error); // Muestra un error en la consola en caso de fallo
+        Alert.alert('Error', data.error); // Muestra una alerta con el mensaje de error
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error:', error); // Muestra un error en la consola en caso de fallo
     } finally {
-      setLoading(false);
-      setRefreshing(false);
+      setLoading(false); // Finaliza el estado de carga
+      setRefreshing(false); // Finaliza el estado de refresco
     }
   };
 
   useEffect(() => {
-    fetchMenuData();
+    fetchMenuData(); // Carga inicial de los datos del carrito al montar el componente
   }, []);
 
+  // Función para manejar el evento de refresco
   const onRefresh = () => {
-    setRefreshing(true);
-    fetchMenuData();
+    setRefreshing(true); // Activa el estado de refresco
+    fetchMenuData(); // Refresca los datos del carrito
   };
 
+  // Calcula el total a pagar sumando el precio por cantidad de cada producto en el carrito
   const totalToPay = cartItems.reduce((total, item) => total + (item.precio_producto * item.cantidad_pedido), 0);
 
   return (
     <ScrollView
       contentContainerStyle={styles.container}
-      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />} // Componente RefreshControl para actualizar la lista
     >
       <Text style={styles.header}>Carrito de compras</Text>
       <TouchableOpacity style={styles.finalizeButton}>
@@ -184,14 +192,17 @@ const CartScreen = () => {
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
+        // Mapea los elementos del carrito y renderiza la información de cada producto
         cartItems.map((item) => (
           <View key={item.id_producto.toString()} style={styles.cartItem}>
             <View style={styles.itemHeader}>
               <Text style={styles.itemName}>{item.descripcion_producto}</Text>
               <View style={styles.iconsContainer}>
+                {/* Botón para editar el producto */}
                 <TouchableOpacity onPress={() => readOne(item.id_detalle_pedido)}>
                   <Ionicons name="pencil" size={24} color="black" />
                 </TouchableOpacity>
+                {/* Botón para eliminar el producto */}
                 <TouchableOpacity onPress={() => deleteDetalle(item.id_detalle_pedido)}>
                   <Ionicons name="trash" size={24} color="black" />
                 </TouchableOpacity>
@@ -208,12 +219,14 @@ const CartScreen = () => {
           </View>
         ))
       )}
+      {/* Modal para editar detalles del producto */}
       <Modal
         animationType="slide"
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => setModalVisible(false)}
       >
+        {/* Overlay para cerrar el modal al hacer clic fuera del contenido */}
         <TouchableOpacity style={styles.modalOverlay} onPress={() => setModalVisible(false)}>
           <View style={styles.modalContent}>
             {loadingModal ? ( // Mostrar círculo de carga mientras se está cargando el modal
@@ -221,6 +234,7 @@ const CartScreen = () => {
             ) : (
               <>
                 <Text style={styles.modalTitle}>Editar Producto</Text>
+                {/* Input para la cantidad */}
                 <TextInput
                   style={styles.modalInput}
                   placeholder="Cantidad"
@@ -228,6 +242,7 @@ const CartScreen = () => {
                   onChangeText={setCantidad}
                   keyboardType="numeric"
                 />
+                {/* TextArea para la nota */}
                 <TextInput
                   style={styles.modalTextArea}
                   placeholder="Nota"
@@ -235,6 +250,7 @@ const CartScreen = () => {
                   onChangeText={setNota}
                   multiline
                 />
+                {/* Botón para confirmar la edición */}
                 <TouchableOpacity style={styles.confirmButton} onPress={() => updateDetalle(currentItemId)}>
                   <Text style={styles.confirmButtonText}>Confirmar</Text>
                 </TouchableOpacity>
@@ -247,8 +263,7 @@ const CartScreen = () => {
   );
 };
 
-
-
+// Estilos para el componente CartScreen
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
