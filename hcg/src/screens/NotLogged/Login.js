91a6807ecab70e09e70dc618241cbfab9394a645
Frank5031 <20220332@ricaldone.edu.sv>
2024-06-15 16:46:00 -0600

    Comentarios en la ventana de logIn

--- hcg/src/screens/NotLogged/Login.js
+++ hcg/src/screens/NotLogged/Login.js
@@ -1,70 +1,52 @@
+// Importación de librerías y componentes necesarios
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Alert,Button, TouchableOpacity,Image, ImageBackground, StyleSheet, KeyboardAvoidingView, Platform,RefreshControl } from 'react-native';
-import { useAuth } from '../../contexts/AuthContext';
-import { useUser } from '../../contexts/UserContext';
-import { SERVER } from '../../contexts/Network';
-import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { useAuth } from '../../contexts/AuthContext'; // Hook del contexto de autenticación
+import { useUser } from '../../contexts/UserContext'; // Hook del contexto de usuario
+import { SERVER } from '../../contexts/Network'; // URL del servidor para realizar solicitudes
+import { useNavigation } from '@react-navigation/native'; // Hook de navegación
+import Icon from 'react-native-vector-icons/FontAwesome'; // Iconos de FontAwesome
 
+// Componente de función Login
 const Login = () => {
-  const [loading, setLoading] = useState(true);
-  const [refreshing, setRefreshing] = useState(false); 
+  // Estados locales
+  const [loading, setLoading] = useState(true); // Estado de carga
+  const [refreshing, setRefreshing] = useState(false); // Estado de actualización
   //const [data, setData] = useState([]);
-  const { setIsLoggedIn } = useAuth();
-  const [search, setSearch] = useState('');
-  const navigation = useNavigation();
-  const { setIdUsuario } = useUser();
-  const [usuario, setUsuario]=useState('')
-  const [clave, setClave] = useState('')
+  const { setIsLoggedIn } = useAuth(); // Método para establecer si el usuario está logueado
+  const [search, setSearch] = useState(''); // Estado para el término de búsqueda
+  const navigation = useNavigation(); // Hook de navegación para cambiar entre pantallas
+  const { setIdUsuario } = useUser(); // Método para establecer el ID del usuario
+  const [usuario, setUsuario]=useState('') // Estado para el nombre de usuario
+  const [clave, setClave] = useState('') // Estado para la contraseña
 
-
+  // Función para manejar la actualización de la pantalla
   const onRefresh = () => {
     setRefreshing(true);
   };
+
+  // Función para manejar el inicio de sesión
   const handleLogin = async () => {
     try {
-      const formData = new FormData();
-      formData.append('usu', usuario);
-      formData.append('clave', clave);
+      const formData = new FormData(); // Crea un nuevo objeto FormData para enviar los datos
+      formData.append('usu', usuario); // Agrega el nombre de usuario al formulario
+      formData.append('clave', clave); // Agrega la contraseña al formulario
       console.log(formData);
+      // Realiza una solicitud POST al servidor para iniciar sesión
       const response = await fetch(`${SERVER}services/public/clientes.php?action=logIn`, {
         method: 'POST',
         body: formData
       });
       
-      const data = await response.json();
-      if (data.status) {
-        Alert.alert('Correcto', data.message);
-        setIsLoggedIn(true);
-        setIdUsuario(response.dataset);
-        navigation.navigate('Main');
+      const data = await response.json(); // Convierte la respuesta en formato JSON
+      if (data.status) { // Si el inicio de sesión es exitoso
+        Alert.alert('Correcto', data.message); // Muestra una alerta con el mensaje de éxito
+        setIsLoggedIn(true); // Establece el estado de logueado en verdadero
+        setIdUsuario(response.dataset); // Guarda el ID del usuario
+        navigation.navigate('Main'); // Navega a la pantalla principal
       } else {
         console.log(data);
-        // Alert the user about the error
-        Alert.alert('Error sesion', data.error);
-      }
-    } catch (error) {
-      console.error(error, "Error desde Catch");
-      
-      Alert.alert('Error', 'Ocurrió un error al iniciar sesión');
-    }
-  };
-  const handleCerrar = async () => {
-    try {
-      const formData = new FormData();
-      formData.append('usu', usuario);
-      formData.append('clave', clave);
-      console.log(formData);
-      const response = await fetch(`${SERVER}services/public/clientes.php?action=logOut`, {
-        method: 'POST',
-        body: formData
-      });
-      
-      const data = await response.json();
-      if (data.status) {
-        Alert.alert('Correcto', data.message);
-      } else {
-        console.log(data);
+        // Muestra una alerta con el mensaje de error
         Alert.alert('Error sesion', data.error);
       }
     } catch (error) {
@@ -74,6 +56,34 @@ const Login = () => {
     }
   };
 
+  // Función para manejar el cierre de sesión
+  const handleCerrar = async () => {
+    try {
+      const formData = new FormData(); // Crea un nuevo objeto FormData para enviar los datos
+      formData.append('usu', usuario); // Agrega el nombre de usuario al formulario
+      formData.append('clave', clave); // Agrega la contraseña al formulario
+      console.log(formData);
+      // Realiza una solicitud POST al servidor para cerrar sesión
+      const response = await fetch(`${SERVER}services/public/clientes.php?action=logOut`, {
+        method: 'POST',
+        body: formData
+      });
+      
+      const data = await response.json(); // Convierte la respuesta en formato JSON
+      if (data.status) { // Si el cierre de sesión es exitoso
+        Alert.alert('Correcto', data.message); // Muestra una alerta con el mensaje de éxito
+      } else {
+        console.log(data); // Imprime la respuesta en la consola
+        Alert.alert('Error sesion', data.error); // Muestra una alerta con el mensaje de error
+      }
+    } catch (error) {
+      console.error(error, "Error desde Catch");
+      
+      Alert.alert('Error', 'Ocurrió un error al iniciar sesión'); // Muestra una alerta con un mensaje genérico de error
+    }
+  };
+
+  // Función para navegar a la pantalla de registro
   const irRegistrar = async () => {
     navigation.navigate('SignUp');
   };
@@ -83,6 +93,7 @@ const Login = () => {
     QuickSandBold: require("../../../assets/fonts/Quicksand-Bold.ttf"),
   });*/
   
+  // Elementos que se muestran en la pantalla
   return (
     <ImageBackground source={require('../../img/fondo.png')} style={styles.backgroundImage}>
       <View style={styles.container}>
@@ -126,6 +137,7 @@ const Login = () => {
   );
 };
 
+// Estilos del componente
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
