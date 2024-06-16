7940a6903e650267a9e2d7b2e0e67f685beafbac
JLea2603 <20220638@ricaldone.edu.sv>
2024-06-16 12:27:08 -0600

    verificación usuario (diseño) completado

--- hcg/src/screens/NotLogged/Recup/VerifCode.js
+++ hcg/src/screens/NotLogged/Recup/VerifCode.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Hook de navegación
+import Ionicons from 'react-native-vector-icons/Ionicons'; // Iconos de Ionicons
 
 const Producto = () => {
+  const navigation = useNavigation(); // Hook de navegación para cambiar entre pantallas
+
   return (
+
     <View style={styles.container}>
-      <Text style={styles.text}>¡Hola, mundo!</Text>
+      {/* Botón para volver a la pantalla anterior (en este caso a la pantalla de login) */}
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Ionicons name="arrow-back" size={40} color="black" />
+      </TouchableOpacity>
+      {/* Título de la pantalla de registro */}
+      <Text style={styles.title}>Recuperación</Text>
+      <Text style={styles.text}>¡Hola, pato!</Text>
     </View>
   );
 };
@@ -12,12 +23,19 @@ const Producto = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 60,
+    padding: 30,
+    backgroundColor: '#d29c65', // Color de fondo
   },
   text: {
+    marginTop: 40,
     fontSize: 24,
+    textAlign: 'center'
+  },
+  title: {
+    fontSize: 30,
     fontWeight: 'bold',
+    textAlign: 'center'
   },
 });
 
--- hcg/src/screens/NotLogged/Recup/VerifUs.js
+++ hcg/src/screens/NotLogged/Recup/VerifUs.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Hook de navegación
 import Ionicons from 'react-native-vector-icons/Ionicons'; // Iconos de Ionicons
 
+const VerifUs = () => {
+  const navigation = useNavigation(); // Hook de navegación para cambiar entre pantallas
+  const [text, setText] = useState(''); // Estado para el texto del input
+  const [placeholderVisible, setPlaceholderVisible] = useState(true); // Estado para controlar la visibilidad del placeholder
 
-const Producto = () => {
+
+
+  const onChangeTextHandler = (inputText) => { // pequeña constante para validar cuando el placeholder se muestre y cuando no 
+    setText(inputText);
+    if (inputText.length > 0) {
+      setPlaceholderVisible(false);  // Ocultar el placeholder cuando se escriba algo
+    } else {
+      setPlaceholderVisible(true);   // Mostrar el placeholder si no hay texto
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* Botón para volver a la pantalla anterior (en este caso a la pantalla de login) */}
@@ -12,6 +27,29 @@ const Producto = () => {
       </TouchableOpacity>
       {/* Título de la pantalla de registro */}
       <Text style={styles.title}>Recuperación</Text>
+      {/* Imagen alusiva a la pantalla*/}
+      <Image
+        style={styles.imagen}
+        source={require('../../../img/Recup1.png')}
+      />
+
+      <Text style={styles.text}>Ingrese su usuario</Text>
+
+      {/* Input para escribir el usuario a recuperar*/}
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeTextHandler} 
+        value={text}
+        placeholder={placeholderVisible ? 'usuario' : ''}
+      />
+
+      {/* Contenedor para alinear solo el botón al centro de la pantalla*/}
+      <View style={styles.containerButton}>
+        {/* Botón de confirmación y agregado para al precionar mandar a la ventana de verificación de codigo */}
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('VerifCode')}> 
+          <Text style={styles.buttonText}>Confirmar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -21,17 +59,51 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 60,
     padding: 30,
-    backgroundColor: '#d29c65', // Background color
+    backgroundColor: '#d29c65', // Color de fondo
   },
   text: {
+    marginTop: 40,
     fontSize: 24,
-    fontWeight: 'bold',
+    textAlign: 'center'
   },
   title: {
-    fontSize : 30,
+    fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center'
+  },
+  imagen: {
+    marginTop: 60,
+    marginStart: 80,
+  },
+  input: {
+    marginTop: 30,
+    height: 60,
+    borderBottomWidth: 2,
+    borderBottomColor: 'black',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    fontSize: 20
+  },
+  button: {
+    height: 50,
+    width: 170,
+    backgroundColor: '#2F2C2C',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30
+  },
+  buttonText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '#B3B3B3',
+    fontSize: 18,
+  },
+  containerButton: {
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
 
-export default Producto;
+export default VerifUs;
