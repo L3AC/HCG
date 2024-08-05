8a6dd0ca458b6c31e243fbb407019d80a731ea84
L3AC <leac.2xy@gmail.com>
2024-08-05 15:53:22 -0600

    update Network.js

--- hcg/src/contexts/Network.js
+++ hcg/src/contexts/Network.js
@@ -2,5 +2,4 @@
 export const SERVER = "http://192.168.1.8:80//HCG/api/";
 //export const SERVER = "http://10.10.0.115:80//HCG/api/";
 //export const SERVER = "http:/192.168.88.157:80//HCG/api/";
-export const SERVER = "http://192.168.1.13:80//HCG/api/";
 //export const SERVER = "http://192.168.1.5:80//HCG/api/";
