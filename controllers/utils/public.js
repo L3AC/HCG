ea2b74feab36c6324428bc4d683855f7960d6ca3
JLea2603 <20220638@ricaldone.edu.sv>
2024-09-23 09:10:35 -0600

    Cambios de sobre nosotros

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -168,7 +168,7 @@ const loadTemplate = async () => {
             <section class="mb-1">
             <!-- Acerca de nosotros -->
             <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="aboutUs.html" role="button"
-            data-mdb-ripple-color="dark"><i class="bi bi-question-circle-fill" style="color: white;"></i></a>
+            data-mdb-ripple-color="dark"><i class="bi bi-info-circle-fill" style="color: white;"></i></a>
 
             <!-- Facebook -->
             <a class="btn btn-link btn-floating btn-lg text-dark m-1" href="https://facebook.com/" role="button"
