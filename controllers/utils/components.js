708981da5fb9103718dba29c5c01be07d50e15cb
L3AC <leac.2xy@gmail.com>
2024-10-08 22:30:21 -0600

    update components.js

--- controllers/utils/components.js
+++ controllers/utils/components.js
@@ -2,8 +2,8 @@
 *   CONTROLADOR DE USO GENERAL EN TODAS LAS PÁGINAS WEB.
 */
 // Constante para establecer la ruta base del servidor.
-//const SERVER_URL = 'https://leac.website/HCG/api/';
-const SERVER_URL = 'http://localhost/HCG/api/';
+const SERVER_URL = 'https://leac.website/HCG/api/';
+//const SERVER_URL = 'http://localhost/HCG/api/';
 
 /*
 *   Función para mostrar un mensaje de confirmación. Requiere la librería sweetalert para funcionar.
